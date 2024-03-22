5d559bcfe3e8fe6a086a1f073f36a1eea68ff836
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-22 09:47:54 -0300

    feat: Adicionar função para carregar dados do arquivo csv

--- 01-logica-de-programacao/projeto2-analise-meteorologica/projeto2_analise_meteorologica.py
+++ 01-logica-de-programacao/projeto2-analise-meteorologica/projeto2_analise_meteorologica.py
@@ -1,11 +1,37 @@
 import csv
 
-nome_do_arquivo = "Anexo_Arquivo_Dados_Projeto_Logica_e_programacao_de_computadores.csv"
+def carregar_dados(nome_do_arquivo):
+    """
+    Carrega os dados de um arquivo CSV para uma lista de dicionários.
 
-with open(nome_do_arquivo, "r", encoding="utf-8") as arquivo:
-    documento = csv.DictReader(arquivo, delimiter=",")
-    dados_meteorologicos = list(documento)
+    Parâmetros:
+    nomo_do_arquivo (str): O nome do arquivo CSV a ser carregado.
 
-# Imprime os primeiros 5 registros
-for registro in dados_meteorologicos[:10]:
-    print(registro)
+    Retorna:
+    list: Uma lista de dicionários, onde cada dicionário representa uma linha do arquivo CSV.
+    """
+    dados = []
+    with open(nome_do_arquivo, 'r', newline='') as arquivo_csv:
+        leitor_csv = csv.DictReader(arquivo_csv)
+        for linha in leitor_csv:
+            dados.append(linha)
+    return dados
+
+def exibir_dados(dados, n):
+    """
+    Exibe os primeiros N registros dos dados.
+
+    Parâmetros:
+    dados (list): Uma lista de dicionários contendo os dados a serem exibidos.
+    n (int): O número de registros a serem exibidos.
+
+    Retorna:
+    None
+    """
+    for i in range(min(n, len(dados))):
+        print(dados[i])
+
+
+nomo_do_arquivo = "Anexo_Arquivo_Dados_Projeto_Logica_e_programacao_de_computadores.csv"
+dados = carregar_dados(nomo_do_arquivo)
+exibir_dados(dados, 10)
